a2f2ccffb64599dc4555eab3e4a852a822aa542c
sparky0904 <sparky0904@gmail.com>
2017-08-21 20:58:49 +0100

    Project Created

---
+++ FiremanJoe/Assets/Scripts/Friendly.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Friendly : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Use this for initialization
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+
+        StartCoroutine(Launch());
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    // Launch the friendly
+    private IEnumerator Launch()
+    {
+        yield return new WaitForSeconds(1);
+
+        rb.isKinematic = false;
+    }
+}
---
+++ FiremanJoe/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float PosX;
+    private Vector3 playerPos;
+
+    public float Speed = 1f;
+    public float ClampLeft = -6f;
+    public float ClampRight = 6f;
+
+    // Use this for initialization
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerPos = transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        PosX = transform.position.x + (Input.GetAxis("Horizontal") * Speed);
+        PosX = Mathf.Clamp(PosX, ClampLeft, ClampRight);
+        playerPos = new Vector3(PosX, playerPos.y, playerPos.z);
+        transform.position = playerPos;
+    }
+}
